5da6db932bccf60fb4e556b3649d56ffe5e5c38f
nicholasborghese <nicholas.borghese@gmail.com>
2019-11-08 17:37:49 -0500

    Connection to API established

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 export default Route.extend({
+  model() {
+    return fetch('/potatotomato.json').then(function(response) {
+      return response.json();
+    });
+  }
 });
